aebc3cdfcb2f250bb37ef9ed23f30767843e5bf6
hb2019 <hb2019@hb2019-PC>
2016-06-22 20:37:55 +0900

    0622

--- oop2/src/grade1/GradeBean.java
+++ oop2/src/grade1/GradeBean.java
@@ -1,56 +1,15 @@
 package grade1;
-/** 
-	 * 클라이언트에서 프로그램 개발 요청이 왔습니다.  
-	 * 이름과 국,영,수 세과목점수를 입력받아서() [홍길동 : 총점 ***점, 
-	 * 평균***점,학점 : F] 을 출력하는 프로그램을 만들어 주세요. 단) 평균은 소수점이하는 절삭합니다 평균점수가 90점 이상 A 
-	 * 80점 이상이면 B 70점 이상이면 C 60점 이상이면 D 50점 이상이면 E 50점 미만이면 F 학점입니다라고 출력되게 해주세요 
-	 * [단]switch-case 문으로 해결하세요 
- */ 
-public class GradeBean {//Bean은 수정하지않음
+
+	public class GradeBean {//Bean은 수정하지않음//저장과 불러오는곳
 	private int kor,eng,math;
 	private String name;
 	
-	public GradeBean(){
-		this.name=name;
-		this.kor=kor;
-		this.eng=eng;
-		this.math=math;
-	}
-	public void setKor(int kor){
-		this.kor=kor;
-	}
-	public void setEng(int eng){
-		this.eng=eng;
-	}
-	public void setMath(int math){
-		this.math=math;
-	}
-	public void setName(String name){
-		this.name=name;
-	}
-	
-	public int getKor(){
-		return kor;
-	}
-	public int getEng(){
-		return eng;
-	}
-	public int getMath(){
-		return math;
-	}
-	public String getName(){
-		return name;
-	}
-	@Override
-	public String toString() {
-		return "결과 [이름=" + name 
-				+ "\n국어=" + kor 
-				+ " \n영어=" + eng 
-				+ "\n수학=" + math ;
-	}
 	
 	
- } 
+	
+	}
+	
+ 
 
 
 
--- oop2/src/grade1/GradeServiceImpl.java
+++ oop2/src/grade1/GradeServiceImpl.java
@@ -1,65 +1,42 @@
 package grade1;
 
-public class GradeServiceImpl implements GradeService {
-	GradeBean bean =new GradeBean();
+class GradeServiceImpl implements GradeService {
+	GradeBean bean=new GradeBean();
 	
-	//과목별 점수체크
+	@Override
+	public void gradeInit(String name, int kor, int eng, int math) {
+		// TODO Auto-generated method stub
+		
+	}
+
 	@Override
 	public String gradeCheck(int kor, int eng, int math) {
- 		if ((kor >= 101 || kor<0)&&(eng >= 101 || eng <0)&&(math >= 101||math<0)) { 
- 			return "재입력";
- 		}else{
- 			return "완료";
- 		}
+		// TODO Auto-generated method stub
+		return null;
 	}
-	//이름 과목별 점수저장
-	@Override
-	public void gradeInit (String name, int kor, int eng, int math) {
-		bean.setName(name);
-		bean.setKor(kor);
-		bean.setEng(eng);
-		bean.setMath(math);
-	}
-	//총점계산
+
 	@Override
 	public int gradeTotal() {
-		int sum = bean.getKor()+bean.getEng()+bean.getMath();
-		return sum;
+		// TODO Auto-generated method stub
+		return 0;
 	}
-	//평균계산
+
 	@Override
 	public int gradeAvg() {
-		int avg=this.gradeTotal()/3;
-		return avg;
+		// TODO Auto-generated method stub
+		return 0;
 	}
-	//학점계산
+
 	@Override
 	public String gradeRank() {
-		int avg= this.gradeAvg();
-				String qwe="";
-		if (avg >= 90) { 
- 			qwe = "A"; 
- 		} else if (avg >= 80) { 
- 			qwe = "B"; 
-		} else if (avg >= 70) { 
-			qwe = "C"; 
- 		} else if (avg >= 60) { 
- 			qwe = "D"; 
-		} else if (avg >= 50) { 
- 			qwe = "E"; 
- 		} else if (avg >= 49) { 
- 			qwe = "F"; 
- 		} else { 
- 		} 
-		
-		return qwe;
+		// TODO Auto-generated method stub
+		return null;
 	}
+
 	@Override
 	public String gradeResult(String result) {
-		return bean.toString()
-				+"\n총점 = "+this.gradeTotal()
-				+"\n평균 = "+this.gradeAvg()
-				+"\n학점 = "+result+"]";
+		// TODO Auto-generated method stub
+		return null;
 	}
 	
 	
