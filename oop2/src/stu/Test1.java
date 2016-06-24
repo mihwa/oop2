5e154b756cadf2060e1d9b099a3e0abd918241cb
hb2019 <hb2019@hb2019-PC>
2016-06-24 16:51:34 +0900

    0623

--- oop2/src/stu/Test1.java
+++ oop2/src/stu/Test1.java
@@ -1,31 +1,8 @@
 package stu;
 
-import javax.swing.JOptionPane;
 
 public class Test1 {
 	public static void main(String[] args) {
 		
-		while (true) {
-			
-			switch (JOptionPane.showInputDialog("1")){
-					
-			case "1":
-				
-				break;
-			case "2":
-				
-				break;
-			case "3":
-				
-				break;
-			case "4":
-				
-				break;
-			
-
-			default:
-				break;
-			}
 		}
 	}
-}
